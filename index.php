a338f115509810f920ca61fc097857245bf79ed4
student <student@internal.rexnet.nz>
2019-07-24 10:00:07 +1200

    class tutorial

--- index.php
+++ index.php
@@ -2,12 +2,47 @@
 
 echo "<H1>This is Demo of PHP XAMPP</H1>";
 
-$name = "gautam";
+$var1 = "tarq";
+$var2 = "rate";
 
-var_dump($name);
+echo similar_text($var1,$var2,$percent);
+for($i = 0; $i < strlen($var1); $i++){
+    $ch = $var1[$i];
+    $found = false;
+    for($n = 0; $n < strlen($var2); $n++){
+        if ($ch == $var2[$n]){
+            $found = true;
+            break;
+        }
+    }
+    if ($found == false){
+        break;
+    } 
+}
+if ($found) {
+    echo "is an anagram";
+} else {
+    echo "nah";
+}
+echo "<br>";
 
-$name = true;
 
-var_dump($name);
+$var3 = "aba";
+$count=0;
+for($i=0;$i<strlen($var3);$i++){
+    $ch1 = $var3[$i];
+    for($j=0;$j<strlen($var3);$j++){
+        if($ch1==$var3[$j]){
+            $count++;
+            //echo $ch1."/".$count."[".$j."]"."<br>";
+        }
+    }
+    if ($count>1){
+        echo $ch1;
+    }
+    $count=0;
+ 
+}
+
 
 ?>
